445316ffbe9fa6922b729a23d0f9cc75c9a1c3d6
saleemullahkhansaleem <salikhanswati@gmail.com>
2025-07-11 17:12:29 +0500

    fix: update Card component border radius and clean up next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +26,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
